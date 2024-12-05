9c02d3bbd11d13141b9b9d0ee7bac0981396514e
Ludivine388 <58701647+Ludivine388@users.noreply.github.com>
2024-12-05 15:10:18 +0100

    Update Station_Details.py

--- OBU Data/code/Station_Details.py
+++ OBU Data/code/Station_Details.py
@@ -8,23 +8,22 @@ import Station_List
 
 # url of service
 url = "http://192.168.20.10:10987/ucuStationDetails"
+while True:
+    # list of IDs to process
+    ID = Station_List.list_all_IDs
+    print(ID)
+    for station in ID:
+        # id of vehicle
+        raw_data = '<ucuGetStationDetails statID="' + station + '" />'
+        # get StationDetails with given ID
+        r = requests.post(url, data=raw_data, headers={'Content-Type': 'application/json'})
 
-# list of IDs to process
-ID = Station_List.list_all_IDs
-
-for station in ID:
-    # id of vehicle
-    raw_data = '<ucuGetStationDetails statID="' + station + '" />'
-    # get StationDetails with given ID
-    r = requests.post(url, data=raw_data, headers={'Content-Type': 'application/json'})
-
-    data = r.text
-    print(data)
-    # save data to log
-    file = open('log_station_details.txt', 'a')
-    # Append the JSON data to the file
-    json.dump(data, file)
-    # Add a newline character to separate entries (optional)
-    file.write('\n \n \n')
-    # Close the file
-    file.close()
+        data = r.text
+        # save data to log
+        file = open('log_station_details.txt', 'a')
+        # Append the JSON data to the file
+        json.dump(data, file)
+        # Add a newline character to separate entries (optional)
+        file.write('\n \n \n')
+        # Close the file
+        file.close()
